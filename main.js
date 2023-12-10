8cd5aa8cc256fbd9200bb357c2fef08a5f658345
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-12-10 12:55:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,13 +13,6 @@ drawn_sketch = "";
 answer_holder = "";
 score = 0;
 
-function updateCanvas() {
-    background("white") ;
-    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
-    Element_of_array = quick_draw_data_set[random_no];
-    document.getElementById("sketch_to_be_drawn").innerHTML = "Sketch To Be Drawn: "+Element_of_array;
-}
-
 function setup() {
     canvas = createCanvas(280,280) ;
     canvas.position(540,200) ;
